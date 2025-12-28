b618ba7682c888397b985ce7a19a77a10bc2cc90
Sherif Dahy <shrifm2017@gmail.com>
2025-12-28 19:10:29 +0200

    roll back

--- NOTE.Solutions.Workspace/apps/shell/module-federation.config.ts
+++ NOTE.Solutions.Workspace/apps/shell/module-federation.config.ts
@@ -1,33 +1,11 @@
 import type { ModuleFederationConfig } from '@nx/module-federation';
 
-// const sharedPackages: Record<string, string> = {
-//   '@angular/core': '21.0.0',
-//   '@angular/common': '21.0.0',
-//   '@angular/common/http': '21.0.0',
-//   '@angular/router': '21.0.0',
-//   'rxjs': '7.8.0',
-// };
-
 const config: ModuleFederationConfig = {
   name: 'systemAdmin',
 
   exposes: {
     './Module': 'apps/systemAdmin/src/app/remote-entry/entry-module.ts',
   },
-
-  // disableNxRuntimeLibraryControlPlugin: true,
-
-  // shared: (name) => {
-  //   if (sharedPackages[name]) {
-  //     return {
-  //       singleton: true,
-  //       strictVersion: true,
-  //       requiredVersion: sharedPackages[name],
-  //     };
-  //   }
-  //   return undefined;
-  // },
-
 
 };
 
--- NOTE.Solutions.Workspace/apps/systemAdmin/module-federation.config.ts
+++ NOTE.Solutions.Workspace/apps/systemAdmin/module-federation.config.ts
@@ -1,13 +1,5 @@
 import type { ModuleFederationConfig } from '@nx/module-federation';
 
-// const sharedPackages: Record<string, string> = {
-//   '@angular/core': '21.0.0',
-//   '@angular/common': '21.0.0',
-//   '@angular/common/http': '21.0.0',
-//   '@angular/router': '21.0.0',
-//   'rxjs': '7.8.0',
-// };
-
 const config: ModuleFederationConfig = {
   name: 'systemAdmin',
 
@@ -15,18 +7,6 @@ const config: ModuleFederationConfig = {
     './Module': 'apps/systemAdmin/src/app/remote-entry/entry-module.ts',
   },
 
-  // disableNxRuntimeLibraryControlPlugin: true,
-
-  // shared: (name) => {
-  //   if (sharedPackages[name]) {
-  //     return {
-  //       singleton: true,
-  //       strictVersion: true,
-  //       requiredVersion: sharedPackages[name],
-  //     };
-  //   }
-  //   return undefined;
-  // },
 };
 
 export default config;
